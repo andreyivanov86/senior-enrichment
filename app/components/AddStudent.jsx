926f757322fe18887b9f7d2285fe25ecf4332cc1
AndreyI <andreyivanov86@gmail.com>
2017-10-19 20:18:17 -0400

    student posed to db

--- app/components/AddStudent.jsx
+++ app/components/AddStudent.jsx
@@ -7,28 +7,32 @@ class AddStudent extends Component {
 
   componentDidMount() {
     this.props.getCampuses();
-    // this.props.postStudent();
+    //this.props.postStudent();
   }
 
   render() {
+    console.log(this.props)
     const campuses = this.props.campuses;
-
     return (
       <div>
         <div> Add Student </div>
-        <form>
+        <div>
+          <label>Select Campus</label>
+          <select name="campusName">
+            {
+              //add campuses to option list
+              campuses && campuses.map(campus => <option key={campus.id}>{campus.name}</option>)
+            }
+          </select>
+        </div>
+        <form onSubmit={this.props.handleSubmit}>
           <input
             placeholder="Enter Name"
+            type="text"
+            name="studentName"
           />
+          <button type="submit">Submit</button>
         </form>
-        <label>Select Campus</label>
-        <select>
-          {
-            campuses && campuses.map(campus => <option key={campus.id}>{campus.name}</option>)
-          }
-          {/* <option>1</option> */}
-        </select>
-        <button>Submit</button>
       </div>
     )
 
@@ -39,7 +43,6 @@ class AddStudent extends Component {
 const mapStateToProps = (state) => {
   return {
     campuses: state.campuses,
-    students: state.students
   }
 }
 
@@ -47,6 +50,13 @@ const mapDispatchToProps = (dispatch) => {
   return {
     getCampuses() {
       dispatch(fetchCampuses())
+    },
+    handleSubmit(event) {
+      event.preventDefault();
+      // console.log(event.target.cmapusName.value)
+      dispatch(postStudent({
+        name: event.target.studentName.value,
+      }))
     }
   }
 }
